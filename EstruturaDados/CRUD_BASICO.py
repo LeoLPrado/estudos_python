fc611ac86bc98a72eef990fb304a2731cd77a4e7
LeoLPrado <leoloprado355@gmail.com>
2026-01-30 22:16:05 -0300

    Novo arquivo de CRUD

---
+++ EstruturaDados/CRUD_BASICO.py
@@ -0,0 +1,42 @@
+db = [
+    {'nome': 'Leo', 'cargo': 'Engenheiro dados'},
+    {'nome': 'Fulano', 'cargo': 'Dev'}
+]
+
+# C reate (criar)
+# R read (ler/listar)
+# U pdate (atualizar)
+# D elete (deletar/excluir)
+
+def add_user(nome, cargo):
+    db.append({'nome': nome, 'cargo': cargo})
+    print(f'Usuario {nome} adicionado')
+
+def get_user():
+    for u in db:
+        print(u)
+
+def update_user(nome, novo_cargo):
+    for u in db:
+        if u['nome'] == nome:
+            u['cargo'] = novo_cargo
+            print(f'Usuario {nome} atualizado')
+
+def delete_user(nome):
+    aux = len(db)
+    i = 0
+    for u in db:
+        if u['nome'] == nome:
+            del db[i]
+            print(f'Usuario {nome} deletado')
+            break
+        i += 1
+    if i == aux:
+        print(f'Usuario {nome} nao encontrado')
+
+if __name__ == '__main__':
+    add_user('Ciclano', 'Gestor')
+    get_user()
+    update_user('Leo', 'Engenheiro Software')
+    get_user()
+    delete_user('Maria')
